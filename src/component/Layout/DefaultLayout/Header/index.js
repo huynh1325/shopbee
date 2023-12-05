4db1c97ce6bbd4a3bb5107952faa888337121cc3
Your Name <you@example.com>
2023-12-05 14:13:10 +0700

    ReactJS

--- src/component/Layout/DefaultLayout/Header/index.js
+++ src/component/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,10 @@
+import classname from "classname";
+import styles from "./Header.module.scss";
+
+const cx = classname.bind(styles);
+
 function Header() {
-  return <h2>Header</h2>;
+  return <header className={cx("wrapper")}></header>;
 }
 
 export default Header;
