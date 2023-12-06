a10208313a802c10d895bc528db7830898786ce6
Your Name <you@example.com>
2023-12-06 11:04:48 +0700

    hi

--- src/component/Layout/DefaultLayout/Header/index.js
+++ src/component/Layout/DefaultLayout/Header/index.js
@@ -1,25 +1,19 @@
-import classname from "classname";
 import styles from "./Header.module.scss";
-
-const cx = classname.bind(styles);
 
 function Header() {
   return (
-    <header className={cx("wrapper")}>
-      <nav className={cx("header_nav")}>
-        <ul className={cx("header_nav_list")}>
-          <li className={cx("header_nav_item")}>Kênh Người Bán</li>
-          <li className={cx("header_nav_item")}>Tải ứng dụng</li>
-          <li className={cx("header_nav_item")}>Kết nối</li>
-        </ul>
-        <ul className={cx("header_nav_list")}>
-          <li className={cx("header_nav_item")}>Thông Báo</li>
-          <li className={cx("header_nav_item")}>Hỗ Trợ</li>
-          <li className={cx("header_nav_item")}>Tiếng Việt</li>
-          <li className={cx("header_nav_item")}>Đăng Ký</li>
-          <li className={cx("header_nav_item")}>Đăng Nhập</li>
+    <header className={styles.wrapper}>
+      <nav className={styles.header_nav}>
+        <ul className={styles.header_navbar_list}>
+          <li className={styles.header_nav_item}>Thông Báo</li>
+          <li className={styles.header_nav_item}>Hỗ Trợ</li>
+          <li className={styles.header_nav_item}>Đăng Ký</li>
+          <li className={styles.header_nav_item}>Đăng Nhập</li>
         </ul>
       </nav>
+      <div>
+        <div></div>
+      </div>
     </header>
   );
 }
